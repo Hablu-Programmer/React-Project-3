88930b12f23a9a09b9b2b386c17b9862ab07d47c
Hablu-Programmer <habluprogrammer@gmail.com>
2024-12-01 14:54:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import MyButton from './Components/HeroArea'
+
+function App() {
+  return (
+    <>
+ 
+     
+      <MyButton /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/HeroArea.jsx
@@ -0,0 +1,12 @@
+import {Button} from "@nextui-org/react";
+
+function MyButton() {
+  return (
+    <Button color="primary">
+      Button
+    </Button>
+  );
+}
+
+
+export default MyButton; 
