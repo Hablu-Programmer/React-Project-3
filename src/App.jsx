711b11d48359e16c8a62c8ae82ed5ca12babaab9
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-12-05 01:09:40 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { HomePage } from "./Components/home";
 import Footer from "./Components/shared/Footer";
 import MyNavbar from "./Components/shared/Navbar";
+// import { EatAndDrinkPage } from "./Components/eat-and-drink";
 
 function App() {
   return (
     <>
       <MyNavbar />
       <HomePage />
+      {/* <EatAndDrinkPage /> */}
       <Footer />
     </>
   );
