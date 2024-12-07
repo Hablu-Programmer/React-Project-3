b6cefc77d8325ed0c5a56ba0d7210643899b4dae
Hablu-Programmer <habluprogrammer@gmail.com>
2024-12-07 13:34:11 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import "./App.css";
-import { HomePage } from "./Components/home";
-import Footer from "./Components/shared/Footer";
-import MyNavbar from "./Components/shared/Navbar";
-// import { EatAndDrinkPage } from "./Components/eat-and-drink";
-
-<<<<<<< HEAD
 
 import './App.css'
 import Carousel from './Components/Carousel'
@@ -16,12 +10,9 @@ import MyModal from './Components/HomePage/Modal'
 import ImageCardSection from './Components/ImgSection'
 import MyNavbar from './Components/Navbar'
 import ThingsCard from './Components/ThingsToDo'
-=======
->>>>>>> 86dba3d37922ebb57141dfbd0987f59ffac5f478
 function App() {
   return (
     <>
-<<<<<<< HEAD
     <MyNavbar />
     <HeroArea />
     <ImageCardSection /> 
@@ -30,14 +21,8 @@ function App() {
     <Carousel />
     <GALARY />
     <Footer />
-    <MyModal />
+
   
-=======
-      <MyNavbar />
-      <HomePage />
-      {/* <EatAndDrinkPage /> */}
-      <Footer />
->>>>>>> 86dba3d37922ebb57141dfbd0987f59ffac5f478
     </>
   );
 }
